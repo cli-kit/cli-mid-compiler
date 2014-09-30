5f0da8f1047e93c22b952a65d68e34b917247e4f
cyberfunk <hello@cyberfunk.org>
2014-09-30 15:57:46 +0800

    Draft update to use new compiler.

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-var load = require('./load');
-var substitute = require('./substitute');
+var compile = require('cli-compiler');
 
-var deprecate = require('cli-deprecate');
-
-module.exports = {
-  load: function() {
-    deprecate('load middleware is deprecated, please use compiler');
-    return load.apply(this, arguments);
-  },
-  substitute: function() {
-    deprecate('substitute middleware is deprecated, please use compiler');
-    return substitute.apply(this, arguments);
+module.exports = function compiler(config) {
+  var conf = this.configure();
+  config = config || conf.compiler;
+  // morph legacy configuration
+  if(!config && conf.load) {
+    config = {};
+    config.input = [conf.load.file];
+    config.definition = conf.load.options;
+    config.cache = conf.load.cache;
+    if(conf.substitute && conf.substitute.enabled !== undefined) {
+      config.replace = conf.substitute.enabled;
+    }
+  }
+  if(!config) return this;
+  return function compiler(req, next) {
+    compile(config, function(err, creq) {
+      if(err) return next(err);
+      req.compilation = creq;
+      next();
+    });
   }
 }
+
+// DEPRECATED
+var loader = require('./load');
+var substitutor = require('./substitute');
+var deprecate = require('cli-deprecate');
+
+function load() {
+  deprecate('load middleware is deprecated, please use compiler');
+  return loader.apply(this, arguments);
+}
+
+function substitute() {
+  deprecate('substitute middleware is deprecated, please use compiler');
+  return substitutor.apply(this, arguments);
+}
+
+module.exports.load = load;
+module.exports.substitute = substitute;
